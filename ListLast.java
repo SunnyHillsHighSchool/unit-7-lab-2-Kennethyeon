e196af4ab0fed45e9bc4932906e54279652c97b3
Kennethyeon1 <600020446@fjuhsd.org>
2021-02-02 08:25:06 +0000

    finished unit 7 lab 2

--- ListLast.java
+++ ListLast.java
@@ -10,6 +10,26 @@ public class ListLast
 {
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+    //set a boolean variable called flag to false
+    boolean flag = false;
+    //if size of list is less than 2 return false
+    if (ray.size() < 2)
+    {
+      return flag;
+    }
+    //set an integer called val equal to the value of the last number in the list
+    int val = ray.get(ray.size()-1);
+    //loop through each number in the list using a for loop
+    for(int i = 0; i < ray.size() - 1; i++)
+    {
+      //if the number is equal to val return true
+      if (ray.get(i) == val)
+      {
+        flag = true;
+        return flag;
+      }
+    }
+    //else return false
+		return false;
 	}
 }
